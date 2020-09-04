8d9ea084f03bd273c22d01bf60102701f51a3ec5
Matias Micchele <matias.micchele@hotmail.com>
2020-09-04 16:37:23 -0300

    Ajustando elementos y medidas en toda la página

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SectionOneComponent } from './components/section-one/section-one.component';
+import { SectionTwoComponent } from './components/section-two/section-two.component';
+import { SectionThreeComponent } from './components/section-three/section-three.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SectionOneComponent,
+    SectionTwoComponent,
+    SectionThreeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
