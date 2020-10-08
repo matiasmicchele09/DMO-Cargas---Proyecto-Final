fc4ec279f5e26e7869b24ec86d95303478989329
Matias Micchele <matias.micchele@hotmail.com>
2020-10-08 15:18:30 -0300

    Reforma estructural, creacion del home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Rutas
+import { APP_ROUTING } from './app.routes';
+
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { SectionOneComponent } from './components/section-one/section-one.component';
-import { SectionTwoComponent } from './components/section-two/section-two.component';
-import { SectionThreeComponent } from './components/section-three/section-three.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SectionOneComponent,
-    SectionTwoComponent,
-    SectionThreeComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
